71d8d450cb58e69f99284c1b22c75f881fe912d1
emsmullen <msmullen@uci.edu>
2026-04-11 02:46:21 +0000

    added data for 2026-04-11 and fetch-csvs.sh script

--- scripts/fetch-csvs.sh
+++ scripts/fetch-csvs.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+
+#pull latest changes from repo
+git pull origin main
+
+#get data from class repo
 wget https://github.com/joachimvandekerckhove/cogs205b-s26/raw/refs/heads/main/modules/02-version-control/files/data.zip -O temp.zip
 unzip temp.zip 
 dt=$(date '+%Y-%m-%d')
 mkdir -p ../data/$dt
 mv *csv ../data/$dt
-git clone ssh://git@github.com:emsmullen/cogs205b.git
-git commit -m "added data for $dt and fetch-csvs.sh script"
 
-#git remote add origin 
-#git branch -M main
+git add ./data/
+git add ./scripts/fetch-csvs.sh
+git commit -m "added data for $dt and fetch-csvs.sh script"
 git push -u origin main
 
