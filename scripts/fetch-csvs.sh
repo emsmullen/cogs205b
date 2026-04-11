a3ceae7dc7c0450c279a65d14029397dda549736
emsmullen <msmullen@uci.edu>
2026-04-11 02:49:38 +0000

    added data for 2026-04-11 and fetch-csvs.sh script

--- scripts/fetch-csvs.sh
+++ scripts/fetch-csvs.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-#pull latest changes from repo
+#pull latest changes from repo 
 git pull origin main
+
+mkdir temp_for_zip_extract
 
 #get data from class repo
 wget https://github.com/joachimvandekerckhove/cogs205b-s26/raw/refs/heads/main/modules/02-version-control/files/data.zip -O temp.zip
-unzip temp.zip 
+unzip temp.zip -d temp_for_zip_extract
+
 dt=$(date '+%Y-%m-%d')
 mkdir -p ../data/$dt
-mv *csv ../data/$dt
+mv temp_for_zip_extract/*csv ../data/$dt
+rm -r temp_for_zip_extract
 
 git add ./data/
 git add ./scripts/fetch-csvs.sh
