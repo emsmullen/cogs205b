653ee8eac40a99388e2e825e8df3c18523f20dc8
emsmullen <msmullen@uci.edu>
2026-04-13 03:30:09 +0000

    added data for 2026-04-13 and fetch-csvs.sh script

--- scripts/fetch-csvs.sh
+++ scripts/fetch-csvs.sh
@@ -3,22 +3,20 @@
 #pull latest changes from repo 
 git pull origin main
 
-#make temp dir to extract zip file into
+#make temporary directory to extract zip file into
 mkdir temp_for_zip_extract
 
-#get data from class repo
+#get data from class repo and unzip the file into the temp directory
 wget https://github.com/joachimvandekerckhove/cogs205b-s26/raw/refs/heads/main/modules/02-version-control/files/data.zip -O temp.zip
-
-#unzip the file into the temp dir
 unzip temp.zip -d temp_for_zip_extract
 
 #get the date and make a new directory in data with that date, then move the csv files into that directory and remove the temporary directory and zip file
 dt=$(date '+%Y-%m-%d')
-echo dt
 mkdir -p data/$dt
 mv temp_for_zip_extract/*csv ./data/$dt
 rm -r temp_for_zip_extract temp.zip
 
+#add, commit, and push specified changes to git repo
 git add ./data/ ./scripts/fetch-csvs.sh
 git commit -m "added data for $dt and fetch-csvs.sh script"
 git push -u origin main
