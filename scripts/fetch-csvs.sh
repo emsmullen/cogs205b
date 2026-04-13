f979bbddbed51727af3a1d4def78983789e891ec
emsmullen <msmullen@uci.edu>
2026-04-13 03:33:44 +0000

    added data for 2026-04-13 and fetch-csvs.sh script

--- scripts/fetch-csvs.sh
+++ scripts/fetch-csvs.sh
@@ -10,7 +10,7 @@ mkdir temp_for_zip_extract
 wget https://github.com/joachimvandekerckhove/cogs205b-s26/raw/refs/heads/main/modules/02-version-control/files/data.zip -O temp.zip
 unzip temp.zip -d temp_for_zip_extract
 
-#get the date and make a new directory in data with that date, then move the csv files into that directory and remove the temporary directory and zip file
+#get the date and make a new directory in data with that date, then move the csv files into that directory and remove the temporary directory and zip file - note that the date refers to UTC
 dt=$(date '+%Y-%m-%d')
 mkdir -p data/$dt
 mv temp_for_zip_extract/*csv ./data/$dt
